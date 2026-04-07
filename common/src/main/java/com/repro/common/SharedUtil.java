ec08e2f82d92f0df33d75470f935de7e250b4e5d
Amy Zhao <amyzhao@netflix.com>
2026-04-07 16:37:07 -0400

    Use commons-text (not bundled with IntelliJ) to reproduce classloader issue
    Guava is bundled with IntelliJ (module-intellij.libraries.guava.jar)
    so it's always accessible via the platform core classloader. To
    reproduce the lib/ classloader issue, we need a library that is NOT
    bundled and only exists in the plugin's lib/ directory.
    
    commons-text (org.apache.commons:commons-text) is a public Maven
    Central library not shipped with IntelliJ. Using it as a static field
    initializer in SharedUtil forces classloading from the plugin's lib/
    during class init, which will fail when common's classloader has no
    parent that includes lib/.

--- common/src/main/java/com/repro/common/SharedUtil.java
+++ common/src/main/java/com/repro/common/SharedUtil.java
@@ -1,15 +1,17 @@
 package com.repro.common;
 
 import com.google.common.collect.ImmutableList;
+import org.apache.commons.text.WordUtils;
 
 public class SharedUtil {
 
-    // Static field forces guava classloading during SharedUtil class init,
-    // which happens in common's classloader. On the thin client, common's
-    // classloader has no parents, so it can't find ImmutableList.
-    public static final ImmutableList<String> ITEMS = ImmutableList.of("hello", "world");
+    // Static field forces classloading of commons-text during class init.
+    // commons-text is NOT bundled with IntelliJ, so it must come from
+    // the plugin's lib/ directory. On thin client, common's classloader
+    // has no parent that includes lib/, causing ClassNotFoundException.
+    public static final String GREETING = WordUtils.capitalize("hello world");
 
     public static ImmutableList<String> getItems() {
-        return ITEMS;
+        return ImmutableList.of(GREETING);
     }
 }
